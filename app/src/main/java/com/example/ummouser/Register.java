43d59ccf2aae0f9699a25660f56cad1e0329eaae
Sakhispot <dlaminisakhile80@gmail.com>
2019-01-27 00:59:37 +0200

    Register and Sign Up screens design complete

--- app/src/main/java/com/example/ummouser/Register.java
+++ app/src/main/java/com/example/ummouser/Register.java
@@ -1,7 +1,9 @@
 package com.example.ummouser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void nextToSignUp(View view){
+
+        Intent i= new Intent(this, SignUp.class);
+        finish();
+        startActivity(i);
+
     }
 }
--- app/src/main/java/com/example/ummouser/SignUp.java
+++ app/src/main/java/com/example/ummouser/SignUp.java
@@ -1,11 +1,9 @@
 package com.example.ummouser;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SignUp extends AppCompatActivity {
 
@@ -13,17 +11,29 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setTitle("Sign-Up");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent i= new Intent(this, Register.class);
+                finish();
+                startActivity(i);
+                return  true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        Intent intent= new Intent(this, Register.class);
+        startActivity(intent);
+    }
 }
