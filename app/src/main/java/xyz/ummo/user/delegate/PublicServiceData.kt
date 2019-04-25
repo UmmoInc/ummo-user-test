160fe121014edddba28144a5bcccc7fd5620d235
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2019-04-25 22:16:31 +0200

    Create a data class for Public Service

---
+++ app/src/main/java/xyz/ummo/user/delegate/PublicServiceData.kt
@@ -0,0 +1,8 @@
+package xyz.ummo.user.delegate
+
+data class PublicServiceData(
+        val serviceName: String,
+        val province: String,
+        val municipality: String,
+        val town: String,
+        val serviceCode: String)
