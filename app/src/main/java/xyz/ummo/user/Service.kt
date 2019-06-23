c2f598831e64eb19d304bdd7a78c8245a9644822
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2019-06-23 22:29:46 +0200

    Changed to kotlin

--- app/src/main/java/xyz/ummo/user/Service.kt
+++ app/src/main/java/xyz/ummo/user/Service.kt
@@ -1,13 +1,4 @@
 package xyz.ummo.user
 
-class Service(val serviceName: String, val serviceDescription: String, val form: String, val personalDocs: String,
-              val cost: String, val duraion: String, private var steps: List<String>?) {
-
-    fun setSteps(steps: Array<String>) {
-        this.steps = steps
-    }
-
-    fun getSteps(): Array<String>? {
-        return steps
-    }
-}
+data class Service(val serviceName: String, val serviceDescription: String, val form: String, val personalDocs: String,
+              val cost: String, val duraion: String, val steps: List<String>?)
