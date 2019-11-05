97eccd0f3940f2f158828e197d551d816ac54ffb
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2019-11-05 05:26:04 +0200

    Added ConfirmService function

---
+++ app/src/main/java/xyz/ummo/user/delegate/ConfirmService.kt
@@ -0,0 +1,23 @@
+package xyz.ummo.user.delegate
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import com.github.kittinunf.fuel.Fuel
+import org.json.JSONObject
+import xyz.ummo.user.R
+
+abstract class ConfirmService(context: Context, service_id:String) {
+    init {
+        val data = JSONObject();
+        data.put("status","DONE")
+        Fuel.put("${context.getString(R.string.serverUrl)}/service/${service_id}")
+                .jsonBody(data.toString())
+                .response { request, response, result ->
+                    (context as Activity).runOnUiThread {
+                        done(response.data,response.statusCode)
+                    }
+                }
+    }
+    abstract fun done(data:ByteArray,code:Int)
+}
