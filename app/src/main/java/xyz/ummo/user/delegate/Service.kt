be8ef4d8b3dfd11e1ba6389a7139a717c6467835
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2019-11-08 04:58:22 +0200

    Fetching only services with status=IN-PROGRESS

--- app/src/main/java/xyz/ummo/user/delegate/Service.kt
+++ app/src/main/java/xyz/ummo/user/delegate/Service.kt
@@ -10,7 +10,7 @@ import xyz.ummo.user.R.string.*
 abstract class Service(context: Context) {
     init {
         val jwt = PreferenceManager.getDefaultSharedPreferences(context).getString("jwt", "jwt")
-        Fuel.get("/service?user=${User.getUserId(jwt)}")
+        Fuel.get("/service?user=${User.getUserId(jwt)}&status=IN-PROGRESS")
                 .response { request, response, result ->
                     Log.e("Services",String(response.data))
                     done(response.data,response.statusCode)
